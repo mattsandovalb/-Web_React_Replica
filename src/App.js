7cdd0353dd1e0396ea181d432a538242469cc4b1
Matt <mattsandovalb@gmail.com>
2022-11-17 11:50:20 +0100

    Discover_page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 //Pages
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Discover from './components/Discover/Discover';
 import Join from './components/Join/Join';
 //Styles
 import './App.scss';
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Discover/>
     </div>
   );
 }
--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,16 +1,48 @@
 import React from "react";
-
-
+import "../Discover/Discover.scss";
+import mic from "../../assets/img/microphone.svg";
+import covers from "../../assets/img/covers.jpg";
+import albums from "../../assets/img/albums.svg";
+import more from "../../assets/img/more.svg";
 
 
 const Discover = () => {
-    return (
-      <main>
-        <h2>Hola</h2>
-      </main>
-    );
-  };
-  
-  export default Discover;
+  return (
+    <section className="discover_sec ">
+      <div className="global_div gap-5">
+        <div className="discover_div">
+          <h3 className="discover_title">Discover new music</h3>
+          <div className="cards_div gap-3">
+            <div className="car">
+              <div className="info_card">
+                <img className="icon" src={mic}></img>
+                <p>Charts</p>
+              </div>
+            </div>
+            <div className="car">
+              <div className="info_card">
+                <img className="icon" src={albums}></img>
+                <p>Albums</p>
+              </div>
+            </div>
+            <div className="car">
+              <div className="info_card">
+                <img className="icon" src={more}></img>
+                <p>More</p>
+              </div>
+            </div>
+          </div>
+          <p className="text">
+            By joining you coan benefit by listening to the latest albums
+            released.
+          </p>
+        </div>
+        <div className="covers_div">
+          <img className="cover" src={covers}></img>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-
+export default Discover;
